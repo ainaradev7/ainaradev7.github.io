a50f3b735a15e013b827e58ff27b89eadf789417
Ainara <ainaradev7@gmail.com>
2023-11-04 17:56:09 +0100

    changing functions

--- AES/AES.js
+++ AES/AES.js
@@ -1,29 +1,7 @@
 
-function initWait() {
-	console.debug("AES: Init");
-	/*
-	if (CurrentScreen == null || CurrentScreen === "Login") {
-		hookFunction("LoginResponse", 0, (args, next) => {
-			console.debug("BCX: Init LoginResponse caught", args);
-			next(args);
-			const response = args[0];
-			if (isObject(response) && typeof response.Name === "string" && typeof response.AccountName === "string") {
-				loginInit(args[0]);
-			}
-		});
-		InfoBeep(`BCX Ready!`);
-		console.log(`BCX Ready!`);
-	} else {
-		console.debug("BCX: Already logged in, init");
-		init();
-	}
-	*/
-}
-
 function test() {
-	console.log("TESTING");
+	console.log("AES: Initiated");
 	console.log(Player.Reputation);
 }
 
-initWait();
-test();
+init();
